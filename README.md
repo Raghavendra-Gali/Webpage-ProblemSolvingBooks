246ff7a226022b590dbfcb83ba563d729c280f7c
Raghavendra Gali <gargvd0@gmail.com>
2018-03-25 19:40:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Webpage-Week1ExamExtension
---
+++ json/booksTest.js
@@ -0,0 +1,67 @@
+
+request.open("GET","problem-solving-books.json",true);
+request.send(null);
+
+var bookJSON;
+ console.log(bookJSON);
+  function displayBook(bookId){
+      var request = new XMLHttpRequest();
+      request.onreadystatechange = function(){
+      if ( request.readyState === 4 && request.status === 200 ) {
+          bookJSON = JSON.parse(request.responseText);
+       book = bookJSON.items[bookId];
+       console.log(book);
+       console.log(book.volumeInfo.title);
+       document.getElementById("booktitle").innerHTML= book.volumeInfo.title;
+       document.getElementById("author").innerHTML = book.volumeInfo.authors[0];
+       if(book.saleInfo.listPrice == null )
+          document.getElementById("price").innerHTML = "Not Available";
+       else
+          document.getElementById("price").innerHTML = book.saleInfo.listPrice.amount;
+       document.getElementById("publisher").innerHTML = book.volumeInfo.publisher;
+       console.log(book.volumeInfo.publisher);
+       document.getElementById("year").innerHTML = book.volumeInfo.publishedDate;
+       console.log(book.volumeInfo.publishedDate);
+       document.getElementById("Description").innerHTML = book.volumeInfo.description;
+       console.log(book.volumeInfo.description);
+       document.getElementById("img").src = book.volumeInfo.imageLinks.thumbnail;
+       document.getElementById("bookpreview").href = book.volumeInfo.previewLink;
+      }
+    }
+  };
+
+displayBook(0);
+
+var currentBook = 0
+function setButtons() {
+  bookCnt = bookJSON.items.length;
+  console.log(bookCnt);
+  
+  if (currentBook == 0)
+    document.getElementById("previous").disabled = true;
+
+  if (currentBook == bookCnt - 1)
+    document.getElementById("Next").disabled = true;
+
+  
+  if (currentBook > 0)
+    document.getElementById("previous").disabled = false;
+
+  
+  if (currentBook < bookCnt - 1)
+    document.getElementById("Next").disabled = false;
+}
+
+setButtons();
+
+
+function prev() {
+  displayBook(--currentBook);
+  setButtons();
+}
+setButtons();
+
+function next() {
+  displayBook(++currentBook);
+  setButtons();
+}
